b8ade151b3660de3e01a6263c11829ee03f02910
Joaquin-Barrera <franplan26@gmail.com>
2025-08-19 18:58:23 -0300

    initial

---
+++ script.js
@@ -0,0 +1,435 @@
+
+const characterImages = {
+  "Kyo": {
+    src: "assets/previews/kyo.png",
+    width: "500px",
+    top: "60px",
+    left: "1050px"
+  },
+  "Benimaru": {
+    src: "assets/previews/benimaru.png",
+    width: "500px",
+    top: "50x",
+    left: "1050px"
+  },
+  "Daimon": {
+    src: "assets/previews/daimon.png",
+    width: "550px",
+    top: "50px",
+    left: "1050px"
+  },
+  "Iori": {
+    src: "assets/previews/iori.png",
+    width: "500px",
+    top: "70px",
+    left: "1080px"
+  },
+  "Mature": {
+    src: "assets/previews/mature.png",
+    width: "550px",
+    top: "40px",
+    left: "1050px"
+  },
+  "Vice": {
+    src: "assets/previews/vice.png",
+    width: "600px",
+    top: "00px",
+    left: "1000px"
+  },
+    "Terry": {
+    src: "assets/previews/terry.png",
+    width: "650px",
+    top: "-25px",
+    left: "1000px"
+  },
+    "Andy": {
+    src: "assets/previews/andy.png",
+    width: "650px",
+    top: "-10px",
+    left: "1000px"
+  },
+    "Joe": {
+    src: "assets/previews/joe.png",
+    width: "650px",
+    top: "-70px",
+    left: "950px"
+  },
+     "Nameless": {
+    src: "assets/previews/nameless.png",
+    width: "1300px",
+    top: "50px",
+    left: "950px"
+  },
+     "Ryo": {
+    src: "assets/previews/ryo.png",
+    width: "600px",
+    top: "110px",
+    left: "930px"
+  },
+     "Robert": {
+    src: "assets/previews/robert.png",
+    width: "650px",
+    top: "60px",
+    left: "950px"
+  },
+     "Yuri": {
+    src: "assets/previews/yuri.png",
+    width: "500px",
+    top: "70px",
+    left: "1000px"
+  },
+     "Athena": {
+    src: "assets/previews/athena.png",
+    width: "530px",
+    top: "-10px",
+    left: "1050px"
+  },
+     "Kensou": {
+    src: "assets/previews/kensou.png",
+    width: "650px",
+    top: "100px",
+    left: "950px"
+  },
+     "Bao": {
+    src: "assets/previews/bao.png",
+    width: "450px",
+    top: "10px",
+    left: "1050px"
+  },
+     "EX Robert": {
+    src: "assets/previews/EXRobert.png",
+    width: "500px",
+    top: "10px",
+    left: "1050px"
+  },
+     "EX Takuma": {
+    src: "assets/previews/EXTakuma.png",
+    width: "450px",
+    top: "0px",
+    left: "1050px"
+  },
+     "Vanessa": {
+    src: "assets/previews/vanessa.png",
+    width: "300px",
+    top: "0px",
+    left: "1100px"
+  },
+     "Seth": {
+    src: "assets/previews/seth.png",
+    width: "450px",
+    top: "-0px",
+    left: "1050px"
+  },
+     "Ramón": {
+    src: "assets/previews/ramon.png",
+    width: "500px",
+    top: "0px",
+    left: "1050px"
+  },
+     "Leona": {
+    src: "assets/previews/leona.png",
+    width: "400px",
+    top: "30px",
+    left: "1050px"
+  },
+     "Ralf": {
+    src: "assets/previews/ralf.png",
+    width: "570px",
+    top: "50px",
+    left: "1000px"
+  },
+   "Clark": {
+    src: "assets/previews/clark.png",
+    width: "450px",
+    top: "0px",
+    left: "1050px"
+  },
+   "EX Kensou": {
+    src: "assets/previews/EXKensou.png",
+    width: "470px",
+    top: "20px",
+    left: "1050px"
+  },
+   "Rugal": {
+    src: "assets/previews/rugal.png",
+    width: "1050px",
+    top: "35px",
+    left: "800px"
+  },
+   "Goenitz": {
+    src: "assets/previews/goenitz.png",
+    width: "520px",
+    top: "-0px",
+    left: "1100px"
+  },
+   "Yashiro": {
+    src: "assets/previews/yashiro.png",
+    width: "450px",
+    top: "17px",
+    left: "1050px"
+  },
+   "Shermie": {
+    src: "assets/previews/shermie.png",
+    width: "550px",
+    top: "25px",
+    left: "1000px"
+  },
+   "Chris": {
+    src: "assets/previews/chris.png",
+    width: "450px",
+    top: "20px",
+    left: "1050px"
+  },
+   "K'": {
+    src: "assets/previews/K'.png",
+    width: "520px",
+    top: "0px",
+    left: "1000px"
+  },
+   "Maxima": {
+    src: "assets/previews/maxima.png",
+    width: "470px",
+    top: "0px",
+    left: "1070px"
+  },
+   "Whip": {
+    src: "assets/previews/whip.png",
+    width: "400px",
+    top: "0px",
+    left: "1100px"
+  },
+   "Krizalid": {
+    src: "assets/previews/krizalid.png",
+    width: "500px",
+    top: "20px",
+    left: "1050px"
+  },
+   "C-Zero": {
+    src: "assets/previews/c-zero.png",
+    width: "450px",
+    top: "20px",
+    left: "1050px"
+  },
+   "Zero": {
+    src: "assets/previews/zero.png",
+    width: "500px",
+    top: "10px",
+    left: "1030px"
+  },
+   "Igniz": {
+    src: "assets/previews/igniz.png",
+    width: "850px",
+    top: "20px",
+    left: "950px"
+  },
+   "Kula": {
+    src: "assets/previews/kula.png",
+    width: "450px",
+    top: "40px",
+    left: "1100px"
+  },
+   "Foxy": {
+    src: "assets/previews/foxy.png",
+    width: "550px",
+    top: "45px",
+    left: "1000px"
+  },
+   "Ángel": {
+    src: "assets/previews/angel.png",
+    width: "350px",
+    top: "35px",
+    left: "1150px"
+  },
+   "Kim": {
+    src: "assets/previews/kim.png",
+    width: "450px",
+    top: "0px",
+    left: "1100px"
+  },
+   "Chang": {
+    src: "assets/previews/chang.png",
+    width: "650px",
+    top: "20px",
+    left: "950px"
+  },
+   "Choi": {
+    src: "assets/previews/choi.png",
+    width: "550px",
+    top: "10px",
+    left: "950px"
+  },
+   "Orochi Yashiro": {
+    src: "assets/previews/oyashiro.png",
+    width: "450px",
+    top: "0px",
+    left: "1070px"
+  },
+   "Orochi Shermie": {
+    src: "assets/previews/oshermie.png",
+    width: "650px",
+    top: "0px",
+    left: "1050px"
+  },
+   "Orochi Chris": {
+    src: "assets/previews/ochris.png",
+    width: "520px",
+    top: "20px",
+    left: "1000px"
+  },
+   "Jhun": {
+    src: "assets/previews/jhun.png",
+    width: "700px",
+    top: "100px",
+    left: "880px"
+  },
+   "Lin": {
+    src: "assets/previews/lin.png",
+    width: "750px",
+    top: "120px",
+    left: "870px"
+  },
+   "Shingo": {
+    src: "assets/previews/shingo.png",
+    width: "450px",
+    top: "20px",
+    left: "1070px"
+  },
+   "Yamazaki": {
+    src: "assets/previews/yamazaki.png",
+    width: "550px",
+    top: "10px",
+    left: "1000px"
+  },
+   "Blue Mary": {
+    src: "assets/previews/bluemary.png",
+    width: "450px",
+    top: "40px",
+    left: "1070px"
+  },
+   "Billy": {
+    src: "assets/previews/billy.png",
+    width: "950px",
+    top: "115px",
+    left: "650px"
+  },
+   "Geeze": {
+    src: "assets/previews/geeze.png",
+    width: "400px",
+    top: "45px",
+    left: "1100px"
+  },
+   "EX Geeze": {
+    src: "assets/previews/EXgeeze.png",
+    width: "640px",
+    top: "20px",
+    left: "900px"
+  },
+   "Heidern": {
+    src: "assets/previews/heidern.png",
+    width: "450px",
+    top: "20px",
+    left: "1070px"
+  },
+   "Takuma": {
+    src: "assets/previews/takuma.png",
+    width: "650px",
+    top: "35px",
+    left: "900px"
+  },
+   "Chin": {
+    src: "assets/previews/chin.png",
+    width: "700px",
+    top: "40px",
+    left: "900px"
+  },
+   "Xiangfei": {
+    src: "assets/previews/xiangfei.png",
+    width: "700px",
+    top: "50px",
+    left: "950px"
+  },
+   "Hinako": {
+    src: "assets/previews/hinako.png",
+    width: "550px",
+    top: "35px",
+    left: "1000px"
+  },
+   "May Lee": {
+    src: "assets/previews/maylee.png",
+    width: "450px",
+    top: "0px",
+    left: "1050px"
+  },
+   "Kusanagi": {
+    src: "assets/previews/kusanagi.png",
+    width: "650px",
+    top: "-32px",
+    left: "1000px"
+  },
+   "King": {
+    src: "assets/previews/king.png",
+    width: "350px",
+    top: "0px",
+    left: "1100px"
+  },
+   "Mai": {
+    src: "assets/previews/mai.png",
+    width: "700px",
+    top: "30px",
+    left: "950px"
+  },
+   "Kasumi": {
+    src: "assets/previews/kasumi.png",
+    width: "670px",
+    top: "40px",
+    left: "900px"
+  },
+  "Kyo-1": {
+    src: "assets/previews/kyo-1.png",
+    width: "580px",
+    top: "40px",
+    left: "950px"
+  },
+  "Kyo-2": {
+    src: "assets/previews/kyo-2.png",
+    width: "580px",
+    top: "35px",
+    left: "950px"
+  },
+
+};
+
+
+document.querySelectorAll("area").forEach(area => {
+  const previewBox = document.getElementById("character-preview");
+  const previewImg = document.getElementById("preview-img");
+
+area.addEventListener("mouseenter", () => {
+  const title = area.title;
+  const data = characterImages[title];
+
+  if (data) {
+    previewImg.src = data.src;
+
+    previewBox.style.top = data.top || "50px";
+    previewBox.style.left = data.left || "1050px";
+    previewImg.style.width = data.width || "auto";
+    previewImg.style.height = "auto";
+
+    previewBox.classList.add("show");
+  }
+});
+
+area.addEventListener("mouseleave", () => {
+  previewBox.classList.remove("show");
+  previewImg.src = ""; // prevenir imagen "vieja" quedando en fadeout
+  previewImg.style.width = "";
+  previewImg.style.height = "";
+});
+
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("page-wrapper").classList.add("fade-in");
+});
+
